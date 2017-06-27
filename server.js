19c5d95695be1d4d0005281f54d180b041017e73
MarianNikolov <marian.nikolov.net@gmail.com>
2017-06-28 00:01:42 +0300

    Changed theme, and fixed route. Added theme libs.

--- server.js
+++ server.js
@@ -2,29 +2,30 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-// public static folder
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
+const logStream = { stream: fs.createWriteStream(path.join(__dirname, 'logs.log'), {flags: 'a'})};
+const logger = morgan( 
+    'combined', 
+    logStream);
+app.use(logger);
 
+// public static folder
+app.use('/static', express.static(__dirname + '/public'));
+app.use('/libs', express.static(__dirname + '/node_modules'));
 
 // Port and environment
 let env = process.env.NODE_ENV || 'development';
 let port = 3000;
-
-if (true) {
-
-}
-else {
-
-}
 
 // Set view engine
 app.set('view engine', 'pug');
@@ -32,12 +33,8 @@ app.set('views', __dirname + '/server/views');
 app.locals.basedir = path.join(__dirname, '/server/views');
 // app.locals.pretty = true; // uncomment this and pug will return unminified HTML response
 
-// Routes
-// app.get('/', function(req, res) {
-//     res.render('home');
-// });
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => res.render('home')); 
 app.get('/home', (req, res) => res.render('home'));
 
 app.get('/bouquets', (req, res) => res.render('bouquets'));
@@ -46,8 +43,8 @@ app.get('/pots', (req, res) => res.render('pots'));
 app.get('/cards', (req, res) => res.render('cards'));
 
 app.get('/cart', (req, res) => res.render('cart'));
-app.get('/delivery', (req, res) => res.render('delivery'));
 app.get('/payment', (req, res) => res.render('payment'));
+app.get('/delivery', (req, res) => res.render('delivery'));
 app.get('/contacts', (req, res) => res.render('contacts'));
 app.get('/product-info/:id', (req, res) => res.render('product-info'));
 
@@ -57,31 +54,5 @@ app.get('/login', (req, res) => res.render('login'));
 
 app.get('/logout', (req, res) => res.redirect('home')); // redirect to home
 
-
-
-// route for test GET
-// const allFlowers = [{ name: 'rose', id: 5 }];
-// app.get('/flowers', function(req, res) {
-//     res.render('flowers', { allFlowers: allFlowers });
-// });
-
-// route for test POST
-// postman POST 
-// header- content-type : application/JSON
-// body - raw 
-// {
-// 	"id": 1,
-// 	"name": "rose"
-// }
-
-// app.post('/flowers', function(req, res) {
-
-//     // flowers.push(req.body);
-//     console.log(req.body);
-//     res.statusCode = '201';
-//     res.statusMessage = 'Test';
-//     res.json({ message: 'Ok' });
-//     res.end();
-// });
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
