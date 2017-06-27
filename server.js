5e3a2862eca95ef7e7bc57b4e87f327ca168bf55
MarianNikolov <marian.nikolov.net@gmail.com>
2017-06-27 13:18:38 +0300

    Added pages for route and translate some titles. Fixed routing.

--- server.js
+++ server.js
@@ -19,6 +19,13 @@ app.use(express.static(__dirname + '/node_modules'));
 let env = process.env.NODE_ENV || 'development';
 let port = 3000;
 
+if (true) {
+
+}
+else {
+
+}
+
 // Set view engine
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/server/views');
@@ -26,24 +33,37 @@ app.locals.basedir = path.join(__dirname, '/server/views');
 // app.locals.pretty = true; // uncomment this and pug will return unminified HTML response
 
 // Routes
-app.get('/', function(req, res) {
-    res.render('index');
-});
+// app.get('/', function(req, res) {
+//     res.render('home');
+// });
 
-app.get('/shop', (req, res) => res.render('shop'));
+app.get('/', (req, res) => res.render('home'));
+app.get('/home', (req, res) => res.render('home'));
+
+app.get('/bouquets', (req, res) => res.render('bouquets'));
+app.get('/baskets', (req, res) => res.render('baskets'));
+app.get('/pots', (req, res) => res.render('pots'));
+app.get('/cards', (req, res) => res.render('cards'));
+
 app.get('/cart', (req, res) => res.render('cart'));
-app.get('/product', (req, res) => res.render('product'));
-app.get('/text-page', (req, res) => res.render('text-page'));
+app.get('/delivery', (req, res) => res.render('delivery'));
+app.get('/payment', (req, res) => res.render('payment'));
+app.get('/contacts', (req, res) => res.render('contacts'));
+app.get('/product-info/:id', (req, res) => res.render('product-info'));
 
-app.get('/Home', function(req, res) {
-    res.render('index');
-});
+app.get('/profile', (req, res) => res.render('profile')); 
+app.get('/register', (req, res) => res.render('register'));
+app.get('/login', (req, res) => res.render('login'));
+
+app.get('/logout', (req, res) => res.redirect('home')); // redirect to home
+
+
 
 // route for test GET
-const allFlowers = [{ name: 'rose', id: 5 }];
-app.get('/flowers', function(req, res) {
-    res.render('flowers', { allFlowers: allFlowers });
-});
+// const allFlowers = [{ name: 'rose', id: 5 }];
+// app.get('/flowers', function(req, res) {
+//     res.render('flowers', { allFlowers: allFlowers });
+// });
 
 // route for test POST
 // postman POST 
@@ -53,14 +73,15 @@ app.get('/flowers', function(req, res) {
 // 	"id": 1,
 // 	"name": "rose"
 // }
-app.post('/flowers', function(req, res) {
 
-    // flowers.push(req.body);
-    console.log(req.body);
-    res.statusCode = '201';
-    res.statusMessage = 'Test';
-    res.json({ message: 'Ok' });
-    res.end();
-});
+// app.post('/flowers', function(req, res) {
+
+//     // flowers.push(req.body);
+//     console.log(req.body);
+//     res.statusCode = '201';
+//     res.statusMessage = 'Test';
+//     res.json({ message: 'Ok' });
+//     res.end();
+// });
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
