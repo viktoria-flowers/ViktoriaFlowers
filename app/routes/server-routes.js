4ff963c27e974b8d3146cd0dc0bd01592cd5c4bd
Martina Shebova <martina2314@gmail.com>
2017-07-07 13:09:23 +0300

    Worked on payments pug

--- app/routes/server-routes.js
+++ app/routes/server-routes.js
@@ -7,6 +7,7 @@ const serverRoutes = function(app) {
     app.get('/cards', (req, res) => res.render('cards'));
     app.get('/cart', (req, res) => res.render('cart'));
     app.get('/payment', (req, res) => res.render('payment'));
+    app.get('/payment-details', (req, res) => res.render('payment-details'));
     app.get('/delivery', (req, res) => res.render('delivery'));
     app.get('/contacts', (req, res) => res.render('contacts'));
     app.get('/product-info/:id', (req, res) => res.render('product-info'));
