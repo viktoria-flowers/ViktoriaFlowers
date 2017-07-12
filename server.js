329aaa122aff895571f69ae3fef5d1b8b770b0af
M-Yankov <pointblank12345@abv.bg>
2017-07-13 00:16:03 +0300

    Try to resolve end of lines for server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const config = require('./config');
 
-// config.connectionString;
+//  config.connectionString;
 
 app.listen(config.port, () => console.log(`Server running on http://localhost:${config.port}`));
