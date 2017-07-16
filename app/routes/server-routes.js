bca098c03fa58e45da6dffe1bf0f87a08e3d8768
M-Yankov <pointblank12345@abv.bg>
2017-07-17 01:24:45 +0300

    Fixed bug after merge.

--- app/routes/server-routes.js
+++ app/routes/server-routes.js
@@ -81,7 +81,7 @@ const serverRoutes = (app, data) => {
     app.get('/contacts', (req, res) => res.render('contacts'));
     app.get('/product-info/:id', (req, res) => res.render('product-info'));
     app.get('/profile', isLoggedIn, (req, res) =>
-        data.getAll(req.user._id).then((user) => {
+        data.items.getAll(req.user._id).then((user) => {
             return res.render('profile', { user: user });
         }));
     app.get('/checkout', (req, res) => res.render('checkout'));
