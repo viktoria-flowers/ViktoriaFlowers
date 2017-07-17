94a36fed0967ce47563c8b7b0dd31261bc4253e4
M-Yankov <pointblank12345@abv.bg>
2017-07-18 00:20:59 +0300

    Set new properties on bouquets

--- data/bouquets-data.js
+++ data/bouquets-data.js
@@ -3,7 +3,14 @@ const BouquetModel = require('./models/bouquete-model');
 
 class BouquetsData extends Basedata {
     constructor(db) {
-       super(db, BouquetModel, BouquetModel);
+        super(db, BouquetModel, BouquetModel);
+    }
+
+    // override base
+    create(bouqueteModel) {
+        bouqueteModel.dateCreated = new Date();
+        bouqueteModel.viewsCount = 0;
+        return super.create(bouqueteModel);
     }
 }
 
