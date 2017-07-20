f2230efe00818e691738ffc8fd681e13310b2e98
Martina Shebova <martina2314@gmail.com>
2017-07-20 18:41:58 +0300

    Small change in checkout logic

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -11,84 +11,97 @@ $('.addToCart').on('click', () => {
      localStorage.setItem("test", JSON.stringify(obj));
 });
 
-let savedData = JSON.parse(localStorage.test),
-    savedDataLen = savedData.length;
+if(localStorage.length > 0){
+    
+    let savedData = JSON.parse(localStorage.test),
+        savedDataLen = savedData.length;
 
-for(let i = 0; i < savedDataLen; i += 1){
+    for(let i = 0; i < savedDataLen; i += 1){
 
-    //Dinamically create the products in cart => get them from localStorage
-    let mainRow = $('<tr />').addClass('rem1');
-        let firstCell = $('<td />').addClass('invert-image');
-            let firstCellAnchor = $('<a />').attr('href', '/');
-            let firstCellImg = $('<img />').addClass('img-responsive').attr('src', savedData[i].imgUrl);
+        //Dinamically create the products in cart => get them from localStorage
+        let mainRow = $('<tr />').addClass('rem1');
+            let firstCell = $('<td />').addClass('invert-image');
+                let firstCellAnchor = $('<a />').attr('href', '/');
+                let firstCellImg = $('<img />').addClass('img-responsive').attr('src', savedData[i].imgUrl);
 
-            firstCellImg.appendTo(firstCellAnchor);
-            firstCellAnchor.appendTo(firstCell);
-            firstCell.appendTo(mainRow);
+                firstCellImg.appendTo(firstCellAnchor);
+                firstCellAnchor.appendTo(firstCell);
+                firstCell.appendTo(mainRow);
 
-        let secondCell = $('<td />').addClass('invert').html(savedData[i].name);
-        secondCell.appendTo(mainRow);
+            let secondCell = $('<td />').addClass('invert').html(savedData[i].name);
+            secondCell.appendTo(mainRow);
 
-        let thirdCell = $('<td />').addClass('invert');    
-            let thirdCellFirstDiv = $('<div />').addClass('quantity');
-                let firstInnerDiv = $('<div />').addClass('quantity-select');
-                let secondInnerDiv = $('<div />').addClass('entry value-minus');
-                let thirdInnerDiv = $('<div />').addClass('entry value');
-                    let thirdInnerDivSpan = $('<span />').html(1);           
-                let fourthInnerDiv = $('<div />').addClass('entry value-plus active');
+            let thirdCell = $('<td />').addClass('invert');    
+                let thirdCellFirstDiv = $('<div />').addClass('quantity');
+                    let firstInnerDiv = $('<div />').addClass('quantity-select');
+                    let secondInnerDiv = $('<div />').addClass('entry value-minus');
+                    let thirdInnerDiv = $('<div />').addClass('entry value');
+                        let thirdInnerDivSpan = $('<span />').html(1);           
+                    let fourthInnerDiv = $('<div />').addClass('entry value-plus active');
+            
+            fourthInnerDiv.appendTo(thirdCellFirstDiv);
+            thirdInnerDivSpan.appendTo(thirdInnerDiv);
+            thirdInnerDiv.appendTo(thirdCellFirstDiv);
+            secondInnerDiv.appendTo(thirdCellFirstDiv);
+            firstInnerDiv.appendTo(thirdCellFirstDiv);
+            thirdCellFirstDiv.appendTo(thirdCell);
+            thirdCell.appendTo(mainRow);
+
+            let fourthCell = $('<td />').addClass('invert');     
+                let fourthCellLabel = $('<label />').html(savedData[i].price + 'лв.').attr('id', 'dynPrice');
+            
+            fourthCellLabel.appendTo(fourthCell);
+            fourthCell.appendTo(mainRow);
+
+            let fifthCell = $('<td />').addClass('invert');
+                let fifthCellDiv = $('<div />').addClass('rem');
+                    let fifthCellDivInner = $('<div />').addClass('close1');
+            
+            fifthCellDivInner.appendTo(fifthCellDiv);
+            fifthCellDiv.appendTo(fifthCell);
+            fifthCell.appendTo(mainRow);
+
+        let tableBody = $('#checkoutBody');
+            mainRow.appendTo(tableBody);
         
-        fourthInnerDiv.appendTo(thirdCellFirstDiv);
-        thirdInnerDivSpan.appendTo(thirdInnerDiv);
-        thirdInnerDiv.appendTo(thirdCellFirstDiv);
-        secondInnerDiv.appendTo(thirdCellFirstDiv);
-        firstInnerDiv.appendTo(thirdCellFirstDiv);
-        thirdCellFirstDiv.appendTo(thirdCell);
-        thirdCell.appendTo(mainRow);
+        //This script is for deleting product from cart
+        //to do delete only row not whole table
+        fifthCell.on('click', function(){
+            $(this).parent().fadeOut('slow', function(c){
+            $(this).parent().remove();
+            });
+        });
 
-        let fourthCell = $('<td />').addClass('invert');     
-            let fourthCellLabel = $('<label />').html(savedData[i].price + 'лв.').attr('id', 'dynPrice');
-        
-        fourthCellLabel.appendTo(fourthCell);
-        fourthCell.appendTo(mainRow);
+        //This script is for quantity increasing and decreasing
+        fourthInnerDiv.on('click', function(){
+            var divUpd = $(this).parent().find('.value');
+                newVal = parseInt(divUpd.text(), 10)+1;
+            divUpd.text(newVal);
+            var dynPrice = $('#dynPrice').html(newVal * savedData[i].price + 'лв.');
+            
+        });
+        //to do formula for substracting product price
+        secondInnerDiv.on('click', function(){
+            var divUpd = $(this).parent().find('.value');
+                newVal = parseInt(divUpd.text(), 10)-1;
+            if(newVal >= 1) {
+                divUpd.text(newVal)
+                var dynPrice = $('#dynPrice').html(savedData[i].price - newVal + 'лв.');  
+            };
 
-        let fifthCell = $('<td />').addClass('invert');
-            let fifthCellDiv = $('<div />').addClass('rem');
-                let fifthCellDivInner = $('<div />').addClass('close1');
-        
-        fifthCellDivInner.appendTo(fifthCellDiv);
-        fifthCellDiv.appendTo(fifthCell);
-        fifthCell.appendTo(mainRow);
+        });
+
+        //to do total sum
+    }
+}else{
+    let container = $('<tr />').addClass('panel panel-default');
+    let panelBody = $('<td />').attr('colspan', 5).addClass('panel-body text-center').html('Вашата количка е празна');
+
+    panelBody.appendTo(container);
 
     let tableBody = $('#checkoutBody');
-        mainRow.appendTo(tableBody);
-    
-    //This script is for deleting product from cart
-    //to do delete only row not whole table
-    fifthCell.on('click', function(){
-        $(this).parent().fadeOut('slow', function(c){
-        $(this).parent().remove();
-        });
-    });
 
-    //This script is for quantity increasing and decreasing
-    fourthInnerDiv.on('click', function(){
-        var divUpd = $(this).parent().find('.value');
-            newVal = parseInt(divUpd.text(), 10)+1;
-        divUpd.text(newVal);
-        var dynPrice = $('#dynPrice').html(newVal * savedData[i].price + 'лв.');
-        
-    });
-    //to do formula for substracting product price
-    secondInnerDiv.on('click', function(){
-        var divUpd = $(this).parent().find('.value');
-            newVal = parseInt(divUpd.text(), 10)-1;
-        if(newVal >= 1) {
-            divUpd.text(newVal)
-            var dynPrice = $('#dynPrice').html(savedData[i].price - newVal + 'лв.');  
-        };
-
-    });
-
-    //to do total sum
+    container.appendTo(tableBody);
 }
+
 
