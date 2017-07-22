aa1741807c45737f8ba10b1a87277bc2b871b957
M-Yankov <pointblank12345@abv.bg>
2017-07-22 18:40:59 +0300

    Added ajax for paging and sorting. Handlers are added in filter.js. Base-data can use sortinh options. Added constants file. Implemented Api route for products. Added pagination helper, extracted more mixins. Changed the list.pug structure

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,3 +1,5 @@
+const { constants, pagination, productTypes } = require('../utils');
+
 const ajaxRequests = (app, data) => {
     app.get('/api', (req, res) => {
         // usersService returns users from datbase(Mongo)
@@ -9,46 +11,47 @@ const ajaxRequests = (app, data) => {
     app.post('/api/subscribe', (req, res) => {
         return res.json({ message: data.emailSubscribers.find() });
     });
+
+    app.get('/api/products/:type*?', function(req, res) {
+        const type = req.params.type;
+        if (type && productTypes.indexOf(type) === -1) {
+            return res.json({ message: `Wrong type: ${type}` });
+        }
+
+        const page = +req.query.page || 1;
+        const sortField = req.query.sortField || constants.DEFAULT_SORT_FIELD;
+        const sortType = req.query.sortType || constants.DEFAULT_SORT_TYPE;
+
+        // This will be initalizated in then() handlers
+        let productsModels = [];
+        const filter = {};
+        if (type) {
+            filter.type = type;
+        }
+
+        const sort = {};
+        sort[sortField] = +sortType;
+
+        return data.products.getAll(filter, sort, page)
+            .then((products) => {
+                productsModels = products;
+                return data.products
+                    .getAll(filter, {}, 1, Number.MAX_SAFE_INTEGER);
+            })
+            .then((allProds) => {
+                const pages = pagination(
+                    allProds.length,
+                    constants.DEFAULT_PAGE_SIZE,
+                    page);
+
+                return res.render('products/list-partial', {
+                    products: productsModels,
+                    pages: pages,
+                });
+            })
+            .catch((err) => res.status(400)
+                            .res.json(err));
+    });
 };
 
 module.exports = ajaxRequests;
-
-// Here are the ajax requests send from the user
-// const items = [{
-//     'name': 'Martina',
-//     'id': 232,
-// }];
-// const { Router } = require('express');
-
-// const userAjaxRequestsRoutes = function(app) {
-//         const router = new Router();
-
-//         router.get('/', (req, res) => {
-//             res.send(items);
-//         })
-//             .post('/', (req, res) => {
-//                 const item = req.body;
-//                 item.id = items.length + 1;
-//                 items.push(item);
-//                 console.log(items);
-//                 res.send(true).status(201);
-//             })
-//             .get('/:id', (req, res) => {
-//                 const id = +req.params.id;
-//                 const wantedItem = items.find((i) => i.id === id);
-
-//                 if (!wantedItem) {
-//                     return res.status(404)
-//                               .send({
-//                                   error: 'Item not found',
-//                               });
-//                 }
-
-//                 return res.send(wantedItem);
-//             });
-
-//         app.use('/api/items', router);
-// };
-
-// module.exports = userAjaxRequestsRoutes;
-
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,3 +1,6 @@
 const authHelper = require('./auth-helper');
+const constants = require('./constants');
+const pagination = require('./pagination');
+const productTypes = require('./productTypes');
 
-module.exports = { authHelper };
+module.exports = { authHelper, constants, pagination, productTypes };
