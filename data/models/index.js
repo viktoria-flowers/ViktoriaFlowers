45d33f0ba4e1c6f4286a73defa8dfbe728938a0b
M-Yankov <pointblank12345@abv.bg>
2017-07-23 12:43:14 +0300

    index.js for /data/models

---
+++ data/models/index.js
@@ -0,0 +1,6 @@
+const ImageModel = require('./image-model');
+const ProductModel = require('./product-model');
+const SubscribeModel = require('./subscribe-model');
+const UserModel = require('./user-model');
+
+module.exports = { ImageModel, ProductModel, SubscribeModel, UserModel };
