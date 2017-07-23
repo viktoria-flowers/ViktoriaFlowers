98c2f32ca9bb1cb0eaa5f52c65401343f2feffca
M-Yankov <pointblank12345@abv.bg>
2017-07-23 12:42:46 +0300

    added more constants

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -2,4 +2,6 @@ module.exports = {
     DEFAULT_PAGE_SIZE: 12,
     DEFAULT_SORT_FIELD: 'title',
     DEFAULT_SORT_TYPE: 1,
+    MISSING_MODEL_ERR: 'missing-model',
+    EMAIL_REGEX: /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/,
 };
