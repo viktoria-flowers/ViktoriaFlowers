8e19abbf05329799b6bf760324aa796c5624b6ea
MarianNikolov <marian.nikolov.net@gmail.com>
2017-07-29 02:48:05 +0300

    implemented search product with AJAX

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,12 @@
 /*globals $ */
-$('body').on('click', '#search-span', function(){
+$('body').on('click', '.input-group-addon', function (e) {
 
+	let form = $(e.target).closest($('form-group'));
+	
+	if(!$('#autocomplete').val()) {
+		e.preventDefault();
+		return;
+	}
+console.log(55555555555);
+	$(form).submit();
 });
