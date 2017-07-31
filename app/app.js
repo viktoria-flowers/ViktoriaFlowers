4f6166c441e51f2714d82527f482469d3efe6f9f
MarianNikolov <marian.nikolov.net@gmail.com>
2017-07-31 21:11:51 +0300

    extract seed initial user from app.js to seed-database.js

--- app/app.js
+++ app/app.js
@@ -1,29 +1,11 @@
 const express = require('express');
-const { authHelper } = require('./utils/index');
+const initialAdmin = require('./utils/seed-database');
 
 const init = (data) => {
     const app = express();
 
-    data.users.getAll({ username: 'admin11' })
-        .then((users) => {
-            if (users.length === 0) {
-                const initialAdmin = {
-                    username: 'admin11',
-                    password: authHelper.makeHashFromPassword('pe6oadmin4eto'),
-                    names: 'admin',
-                    phone: '0888888888',
-                    email: 'abv@abv.bg',
-                    contactInfo: 'admin contact info',
-                    roles: ['admin'],
-                };
-
-                data.users.collection.insert(initialAdmin);
-            }
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
-
+    initialAdmin.seedDatabase(data);
+    
     const controllers = require('./controllers/init').init(data);
 
     // folder app/config
